efd052177fa4b5e4c05c98905f61f746b0562a4c
Dan Young <dan.young12@gmail.com>
2017-08-24 18:15:09 -0400

    Add project files.

---
+++ RMSMPBackgroundApp/StartupTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Http;
+using Windows.ApplicationModel.Background;
+
+// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
+
+namespace RMSMPBackgroundApp
+{
+    public sealed class StartupTask : IBackgroundTask
+    {
+        public async void Run(IBackgroundTaskInstance taskInstance)
+        {
+            // Create the deferral by requesting it from the task instance.
+            BackgroundTaskDeferral deferral = taskInstance.GetDeferral();
+
+            //
+            // Call asynchronous method(s) using the await keyword.
+            //var result = await ExampleMethodAsync();
+
+            // Once the asynchronous method(s) are done, close the deferral.
+            deferral.Complete();
+        }
+
+
+
+    }
+}
